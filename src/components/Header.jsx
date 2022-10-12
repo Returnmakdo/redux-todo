e468facd1432878655c865e9927862f8c6f69478
Returnmakdo <cldud970@naver.com>
2022-10-12 10:01:12 +0900

    redux-todo

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <StContainer>
+      <div>My Todo List</div>
+      <div>React</div>
+    </StContainer>
+  );
+};
+export default Header;
+
+const StContainer = styled.div`
+  background-color: blueviolet;
+  border: 1px solid #ddd;
+  border-radius: 12px;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  margin-bottom: 24px;
+`;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import Layout from "../components/Layout";
+import Header from "../components/Header";
+import Form from "../components/Form";
+import List from "../components/List";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Header />
+      <Form />
+      <List />
+    </Layout>
+  );
+};
+
+export default Home;
